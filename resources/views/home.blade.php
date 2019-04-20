ad3373c955b2486b992d3d656c7d88c5d188c863
Timothée JEZEK <timothee.jezek@gmail.com>
2019-04-20 11:51:31 +0200

    Add navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+<header>
+@include('layouts.nav')
+</header>
+
+@section('content')
+
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,27 @@
+<nav>
+        <a href="#"><img class="logo" src="{{ url('img/logo_gamezone.png') }}" alt="gamezone"></a>
+        <ul id="menu">
+            <li><a class="border" href="#">Attractions</a></li>
+            <li><a class="border" href="#">Préparer ma visite</a>
+                <ul>
+                    <li><a href="#">Se rendre au parc</a></li>
+                    <li><a href="#">Tarif et billetterie</a></li>
+                    <li><a href="#">Horaires & calendrier</a></li>
+                </ul>
+            </li>
+            <li><a class="border" href="#">Plan</a></li>
+            <li><a class="game" href="#">My game</a></li>
+        </ul>
+        <ul id="langue">
+            <li><a href="#"><img src="{{ url('img/flags/fr.png') }}" alt="Français"></a>
+                <ul>
+                    <li><a href="#"><img src="{{ url('img/flags/en.png') }}" alt="Anglais"></a></li>
+                    <li><a href="#"><img src="{{ url('img/flags/es.png') }}" alt="Espagnol"></a></li>
+            </li>
+        </ul>
+        </ul>
+        <div class="avatar">
+            <span><a href="#">Mon compte</a></span>
+            <img src="{{ url('img/avatar_snake.png') }}" alt="Snake">
+        </div>
+</nav>
