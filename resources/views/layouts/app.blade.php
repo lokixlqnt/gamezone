14dfcc99e52da65bb99017ab40eeff8d1f65ecf0
Thomas Tondeur <thomasinbac@gmail.com>
2019-04-21 18:10:28 +0200

    loic pull moi ça

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,16 @@
     <link rel="stylesheet" href="{{ url('css/main.css') }}">
     <link rel="stylesheet" href="{{ url ('css/compte.css')}}">
     <link rel="stylesheet" href="{{ url ('css/cgv.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+
+
+<!-- Scripts -->
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+
+{!! $calendar_details->script() !!}
+
 
 </head>
 
