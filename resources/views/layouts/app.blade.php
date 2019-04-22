0fac665ca36bcea3c2aad6f7d2242c55146f38b7
Timothée JEZEK <timothee.jezek@gmail.com>
2019-04-22 14:14:30 +0200

    Add Home
    Manque responsive sur quelques élements dont footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,38 @@
 </head>
 
 <body>
-        @yield('content')
+    @yield('content')
+
+    <footer>
+        <div class="left_footer">
+            <ul>
+                <a href="#">
+                    <li>A propos du Parc</li>
+                </a>
+                <a href="#">
+                    <li>Dans la Presse</li>
+                </a>
+            </ul>
+            <div class="footer_recrute">
+                <ul>
+                    <a href="#">
+                        <li>On Recrute</li>
+                    </a>
+                    <a href="#">
+                        <li>Nous Contacter</li>
+                    </a>
+                </ul>
+            </div>
+            <ul>
+                <a href="#">
+                    <li>Conditions de vente</li>
+                </a>
+                <a href="#">
+                    <li>Conditions légales</li>
+                </a>
+            </ul>
+        </div>
+    </footer>
 </body>
 
 </html>
