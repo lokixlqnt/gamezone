381c46534bd077b0e314ac757a9361017e0f2115
Timothée JEZEK <timothee.jezek@gmail.com>
2019-04-24 21:08:32 +0200

    Update footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,23 @@
 
     <footer>
         <div class="left_footer">
-            <ul>
+        <p>Rejoignez nous sur nos réseaux</p>
+            <div class="social">
+                <a href="#"><img src="{{ url('img/social/insta.svg') }}" alt="Instagram"></a>
+                <a href="#"><img src="{{ url('img/social/youtube.svg') }}" alt="Youtube"></a>
+                <a href="#"><img src="{{ url('img/social/facebook.svg') }}" alt="Facebook"></a>
+                <a href="#"><img src="{{ url('img/social/twitter.svg') }}" alt="Twitter"></a>
+            </div>
+            <p class="number">08 59 62 08 59</p>
+            <hr />
+            <div class="hour">Du lundi au jeudi : 9h-19h
+                Du vendredi au samedi : 9h-20h
+                Le dimanche : 9h – 18h
+            </div>
+        </div>
+
+        <div class="right_footer">
+        <ul>
                 <a href="#">
                     <li>A propos du Parc</li>
                 </a>
@@ -41,22 +57,6 @@
                     <li>Conditions légales</li>
                 </a>
             </ul>
-        </div>
-
-        <div class="right_footer">
-        <p>Rejoignez nous sur nos réseaux</p>
-            <div class="social">
-                <a href="#"><img src="{{ url('img/social/insta.svg') }}" alt="Instagram"></a>
-                <a href="#"><img src="{{ url('img/social/youtube.svg') }}" alt="Youtube"></a>
-                <a href="#"><img src="{{ url('img/social/facebook.svg') }}" alt="Facebook"></a>
-                <a href="#"><img src="{{ url('img/social/twitter.svg') }}" alt="Twitter"></a>
-            </div>
-        <p class="number">08 59 62 08 59</p>
-        <hr/>
-        <div class="hour">Du lundi au jeudi : 9h-19h
-Du vendredi au samedi : 9h-20h
-Le dimanche : 9h – 18h
-        </div>
         </div>
     </footer>
 </body>
